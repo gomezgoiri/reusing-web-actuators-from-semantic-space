ef2d730ba22f2da44fae1350147d0ac3b71df80f
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-10-07 16:01:36 +0200

    Fixing tests according to the last refactor

--- tests/consumer.py
+++ tests/consumer.py
@@ -7,20 +7,64 @@ import unittest
 from random import shuffle
 from mock import MagicMock
 from actuation.api.rest import RESTProvider
-from actuation.impl.rest.lamp.mock.consumer import LampConsumerRESTMock
+from actuation.impl.rest.lamp.mock.discovery import MockDiscovery
+from actuation.impl.rest.lamp.mock.agents import Crawler
 
 
-class LampConsumerRESTMockTest(unittest.TestCase):
+# Through these 2 classes, we indirectly test LampConsumerRESTMock
 
+class CrawlerTest(unittest.TestCase):
 
     def setUp(self):
-        self.lc = LampConsumerRESTMock("", "", None)
+        self.discovery = MockDiscovery()        
+        self.discovery.add_discovered( ProvidersFactory.create_mock_providers(), "fakename" )
+        self.crawler= Crawler("", self.discovery)
+    
+    def test_obtain_resource_descriptions(self):
+        self.crawler._obtain_resource_descriptions()
+        
+        expected = ["desc%d"%i for i in range(1,4)]
+        expected.extend( ["desc1%d"%i for i in range(4,6)] )
+        expected.extend( ["desc2%d"%i for i in range(4,6)] )
+        self.assertItemsEqual( expected, self.crawler.descriptions )
 
 
-    def tearDown(self):
+class DiscoveryMockTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.discovery = MockDiscovery()
+    
+    def _create_node(self, name):
+        ret = ProvidersFactory.create_mock_providers()
+        self.discovery.add_discovered( ret, name )
+        return ret
+    
+    def test_get_node(self):
+        nodes = []
+        for name in ("node1.deusto.es" , "node2.deustotech.eu" , "node.morelab.deusto.es"):
+            nodes.append( self._create_node( name ) )
+        
+        self.assertEquals( self.discovery.get_node( "http://node1.deusto.es/res1" ), (nodes[0], "/res1") )
+        self.assertEquals( self.discovery.get_node( "http://node2.deustotech.eu/res4/" ), (nodes[1], "/res4/") )
+        self.assertEquals( self.discovery.get_node( "http://node.morelab.deusto.es/lamp/light" ), (nodes[2], "/lamp/light") )
+        self.assertIsNone( self.discovery.get_node( "http://node.unexisting.com/resource/1" ) )
+
+
+
+# I don't directly use MagicMock, because I need it to be a child of RESTProvider
+class FakeProvider(RESTProvider):
+    
+    def start(self):
+        pass
+    
+    def stop(self):
         pass
 
-    def _create_mock_resources(self):
+
+class ProvidersFactory(object):
+    
+    @staticmethod
+    def _create_mock_resources():
         resources = []
         
         for i in range(1,4): # one rule returned
@@ -39,45 +83,14 @@ class LampConsumerRESTMockTest(unittest.TestCase):
         
         shuffle(resources)
         return resources
-
-    def _create_mock_provider(self):
+    
+    @staticmethod
+    def create_mock_providers():
         provider = FakeProvider()
         provider.get_all_resources = MagicMock()
-        provider.get_all_resources.return_value = self._create_mock_resources()
+        provider.get_all_resources.return_value = ProvidersFactory._create_mock_resources()
         return provider
-    
-    def test_obtain_resource_descriptions(self):
-        self.lc.discover( self._create_mock_provider(), "fakename" )
-        self.lc._obtain_resource_descriptions()
-        
-        expected = ["desc%d"%i for i in range(1,4)]
-        expected.extend( ["desc1%d"%i for i in range(4,6)] )
-        expected.extend( ["desc2%d"%i for i in range(4,6)] )
-        self.assertItemsEqual( expected, self.lc.descriptions )
-    
-    def test_get_node(self):
-        nodes = []
-        for _ in range(3):
-            nodes.append( self._create_mock_provider() )
-        
-        self.lc.discover( nodes[0], "node1.deusto.es" )
-        self.lc.discover( nodes[1], "node2.deustotech.eu" )
-        self.lc.discover( nodes[2], "node.morelab.deusto.es" )
-        
-        self.assertEquals( self.lc.get_node( "http://node1.deusto.es/res1" ), (nodes[0], "/res1") )
-        self.assertEquals( self.lc.get_node( "http://node2.deustotech.eu/res4/" ), (nodes[1], "/res4/") )
-        self.assertEquals( self.lc.get_node( "http://node.morelab.deusto.es/lamp/light" ), (nodes[2], "/lamp/light") )
-        self.assertIsNone( self.lc.get_node( "http://node.unexisting.com/resource/1" ) )
 
-
-# I don't directly use MagicMock, because I need it to be a child of RESTProvider
-class FakeProvider(RESTProvider):
-    
-    def start(self):
-        pass
-    
-    def stop(self):
-        pass
 
 
 if __name__ == "__main__":
