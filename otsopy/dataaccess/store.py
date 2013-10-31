c9fbc16723cd4c46b74ec7a5f3abc1d795f70ad1
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-10-31 16:13:47 +0000

    Adding license warning to OtsoPy's modules

--- otsopy/dataaccess/store.py
+++ otsopy/dataaccess/store.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+'''
+ Copyright (C) 2013 onwards University of Deusto
+  
+ All rights reserved.
+ 
+ This software is licensed as described in the file COPYING, which
+ you should have received as part of this distribution.
+ 
+ This software consists of contributions made by many individuals, 
+ listed below:
+ 
+ @author: Aitor Gómez Goiri <aitor.gomez@deusto.es>
+          Pablo Orduña <pablo.orduna@deusto.es>
+'''
+
 import threading
 import random
 
--- otsopy/dataaccess/util.py
+++ otsopy/dataaccess/util.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+ Copyright (C) 2013 onwards University of Deusto
+  
+ All rights reserved.
+ 
+ This software is licensed as described in the file COPYING, which
+ you should have received as part of this distribution.
+ 
+ This software consists of contributions made by many individuals, 
+ listed below:
+ 
+ @author: Pablo Orduña <pablo.orduna@deusto.es>
+'''
+
 def locked(f):
     def wrapped(self, *args, **kwargs):
         self._lock.acquire()
